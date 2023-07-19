4500dbe91d96e9c11867228ba7a5ebf668829f60
swetasrivastav <swetasri592@gmail.com>
2023-07-20 01:28:43 +0530

    change image

--- components/Home.js
+++ components/Home.js
@@ -31,7 +31,7 @@ const Home = () => {
       </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-  <Image src={Image02} alt="Picture of background" className="w-full" />
+  <Image src={Image02} alt="Picture background" className="w-full" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
@@ -48,7 +48,8 @@ const Home = () => {
       </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-  <Image src={Image06}  alt="Picture of background" className="w-full" />
+  <Image src={Image06} alt="Picture background" className="w-full" />
+
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
@@ -65,7 +66,8 @@ const Home = () => {
       </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-  <Image src={Image04}  alt="Picture of background" className="w-full" />
+  <Image src={Image04} alt="background image" className="w-full" />
+
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
